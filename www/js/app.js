caf13d75fa7545b98cba4a4d6be59478f56e37a9
Adrien Nicolet <adrien.nicolet@gmail.com>
2015-08-27 14:16:26 +0200

    basic navigation

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,33 @@
-// Ionic Starter App
+// Ionic tricountlike App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'tricountlike' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+var app = angular.module('tricountlike', ['ionic'])
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider.state('home', {
+            url: '/home',
+            templateUrl: 'html/home.html'
+        });
+        $stateProvider.state('help', {
+            url: '/help',
+            templateUrl: 'html/help.html'
+        });
+    });
